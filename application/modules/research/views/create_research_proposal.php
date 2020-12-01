e517f9f0ca69b3a9c906f000e1c76d343e7453ea
Prince <princejomino@gmail.com>
2020-12-01 09:08:28 +0800

    Fix Adding Research Proposal

--- application/modules/research/views/create_research_proposal.php
+++ application/modules/research/views/create_research_proposal.php
@@ -8,74 +8,72 @@
 	<div class="card m-b-30">
 		<div class="card-body">
 			<h5 class="header-title pb-3">Fill out the forms</h5>           
-			<div class="general-label">
-				<form role="form">
-					<div class="form-group">
-						<label for="title">Title</label>
-						<input type="text" class="form-control" id="title" placeholder="Enter Title">
-					</div>
-					<div class="form-group" id="proponent-inputs">
-						<label for="proponent">Name of Proponent/s</label>
-						<div class="input-group mt-2">
-							<input type="text" class="form-control" id="proponent" placeholder="Enter Name of Proponent" name="proponent[]">
-                            <span class="input-group-append">
-                                <button type="button" name="add" id="add-proponents-input" class="btn btn-success">Add Proponent</button>
-                            </span>
-                        </div>
-					</div>
-					<div class="form-group">
-						<label for="college-affiliation">College Affiliation</label>
-						<input type="text" class="form-control" id="college-affiliation" placeholder="Enter College Affiliation">
-					</div>
-					<div class="form-group">
-						<label for="background-study">Background of the Study</label>
-						<input type="text" class="form-control" id="background-study" placeholder="Enter Background of the Study">
-					</div>
-					<div class="form-group">
-						<label for="significant">Significant of the Study</label>
-						<input type="text" class="form-control" id="significant" placeholder="Enter Significant of the Study">
-					</div>
-					<div class="form-group">
-						<label for="definition-of-terms">Definition of Terms</label>
-						<input type="text" class="form-control" id="definition-of-terms" placeholder="Enter Definition of Terms">
-					</div>
-					<div class="form-group">
-						<label for="methodology">Methodology</label>
-						<input type="text" class="form-control" id="methodology" placeholder="Enter Methodology">
-					</div>
-					<div class="form-group">
-						<label for="work-plan">Work Plan (Gantt Chart)</label>
-						<input type="text" class="form-control" id="work-plan" placeholder="Enter Work Plan (Gantt Chart)">
-					</div>
-					<div class="form-group">
-						<label for="budget">Line Item Budget</label>
-						<input type="text" class="form-control" id="budget" placeholder="Enter Budget">
-					</div>
-					<div class="form-group" id="item-budget-inputs">
-						<label for="budget">Line Item Budget</label>
-						<input type="text" class="form-control" id="budget" placeholder="Enter Budget">
-						<div class="input-group mt-2 col-sm-4">
-							<input type="text" class="form-control" id="budget" placeholder="Enter Name of Proponent" name="budget[]">
-                            <span class="input-group-append">
-                                <button type="button" name="add" id="add-budget-input" class="btn btn-success">Add Item Budget</button>
-                            </span>
-                        </div>
-					</div>
-					<div class="form-group">
-						<label for="credentials">Credentials of Key Personel/Staff Involved</label>
-						<input type="text" class="form-control" id="credentials" placeholder="Enter Credentials of Key Personel/Staff Involved">
-					</div>
-					<div class="form-group">
-						<label for="references">References</label>
-						<input type="text" class="form-control" id="references" placeholder="Enter References">
-					</div>
-					<div class="form-group">
-						<label for="prepared-by">Prepared By</label>
-						<input type="text" class="form-control" id="prepared-by" placeholder="Enter Prepared By">
-					</div>
-					<button type="submit" class="btn btn-primary">Submit</button>
-				</form>                                    
-			</div>                            
+			<form role="form">
+				<div class="form-group">
+					<label for="title">Title</label>
+					<input type="text" class="form-control" id="title" placeholder="Enter Title">
+				</div>
+				<div class="form-group" id="proponent-inputs">
+					<label for="proponent">Name of Proponent/s</label>
+					<div class="input-group">
+						<input type="text" class="form-control" id="proponent" placeholder="Enter Name of Proponent" name="proponent[]">
+                        <span class="input-group-append">
+                            <button type="button" name="add" id="add-proponents-input" class="btn btn-success">Add Proponent</button>
+                        </span>
+                    </div>
+				</div>
+				<div class="form-group">
+					<label for="college-affiliation">College Affiliation</label>
+					<input type="text" class="form-control" id="college-affiliation" placeholder="Enter College Affiliation">
+				</div>
+				<div class="form-group">
+					<label for="background-study">Background of the Study</label>
+					<textarea class="form-control" id="background-study" placeholder="Enter Background of the Study"></textarea>
+				</div>
+				<div class="form-group">
+					<label for="significant">Significant of the Study</label>
+					<input type="text" class="form-control" id="significant" placeholder="Enter Significant of the Study">
+				</div>
+				<div class="form-group">
+					<label for="definition-of-terms">Definition of Terms</label>
+					<input type="text" class="form-control" id="definition-of-terms" placeholder="Enter Definition of Terms">
+				</div>
+				<div class="form-group">
+					<label for="methodology">Methodology</label>
+					<input type="text" class="form-control" id="methodology" placeholder="Enter Methodology">
+				</div>
+				<div class="form-group">
+					<label for="work-plan">Work Plan (Gantt Chart)</label>
+					<input type="text" class="form-control" id="work-plan" placeholder="Enter Work Plan (Gantt Chart)">
+				</div>
+				<div class="form-group">
+					<label for="budget">Line Item Budget</label>
+					<input type="text" class="form-control" id="budget" placeholder="Enter Budget">
+				</div>
+				<div class="form-group" id="item-budget-inputs">
+					<label for="budget">Line Item Budget</label>
+					<div class="input-group">
+						<input type="text" class="form-control" id="budget-item" placeholder="Enter Budget Item" name="budgetitem[]">
+						<input type="text" class="form-control" id="budget-amount" placeholder="Enter Budget Amount" name="budgetamount[]">
+                        <span class="input-group-append">
+                            <button type="button" name="addbudget" id="add-budget-input" class="btn btn-success">Add Item Budget</button>
+                        </span>
+                    </div>
+				</div>
+				<div class="form-group">
+					<label for="credentials">Credentials of Key Personel / Staff Involved</label>
+					<input type="text" class="form-control" id="credentials" placeholder="Enter Credentials of Key Personel / Staff Involved">
+				</div>
+				<div class="form-group">
+					<label for="references">References</label>
+					<input type="text" class="form-control" id="references" placeholder="Enter References">
+				</div>
+				<div class="form-group">
+					<label for="prepared-by">Prepared By</label>
+					<input type="text" class="form-control" id="prepared-by" placeholder="Enter Prepared By">
+				</div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
 		</div>
 	</div>
 </div>
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,12 +2,22 @@ $(document).ready(function(){
 	var i=1;
 	$('#add-proponents-input').click(function(){
 		i++;
-		$('#proponent-inputs').append('<div class="input-group mt-2" id="row'+i+'"><input type="text" class="form-control" id="proponent'+i+'" placeholder="Enter Name of Proponent" name="proponent[]"><span class="input-group-append"><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">Del</button></span></div>');
+		$('#proponent-inputs').append('<div class="input-group mt-2" id="rowproponent'+i+'"><input type="text" class="form-control" id="proponent'+i+'" placeholder="Enter Name of Proponent" name="proponent[]"><span class="input-group-append"><button type="button" name="removeproponent" id="'+i+'" class="btn btn-danger btn_remove_proponent">Del</button></span></div>');
 	});
 	
-	$(document).on('click', '.btn_remove', function(){
+	$(document).on('click', '.btn_remove_proponent', function(){
 		var button_id = $(this).attr("id"); 
-		$('#row'+button_id+'').remove();
+		$('#rowproponent'+button_id+'').remove();
+	});
+	var j=1;
+	$('#add-budget-input').click(function(){
+		j++;
+		$('#item-budget-inputs').append('<div class="input-group mt-2" id="rowbudget'+j+'"><input type="text" class="form-control" id="budget-item'+j+'" placeholder="Enter Budget Item" name="budgetitem[]"><input type="text" class="form-control" id="budget-amount'+j+'" placeholder="Enter Budget Amount" name="budgetamount[]"><span class="input-group-append"><button type="button" name="removebudget" id="'+j+'" class="btn btn-danger btn_remove_budget">Del</button></span></div>');
+	});
+	
+	$(document).on('click', '.btn_remove_budget', function(){
+		var button_id = $(this).attr("id"); 
+		$('#rowbudget'+button_id+'').remove();
 	});
 	
 	$('#submit').click(function(){		
